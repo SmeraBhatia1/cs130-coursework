e8189512293014bb8a8134784ee9cdd29e17e5b5
smera bhatia <smerabhatia@smeras-MacBook-Pro-2.local>
2021-05-26 20:13:06 -0500

    Checking in my hw04 folder

--- hw04/your_task/js/scripts.js
+++ hw04/your_task/js/scripts.js
@@ -16,18 +16,103 @@ const search = (ev) => {
     }
 }
 
+const playTrack = (ev) => {
+  //  console.log(ev.currentTarget);
+    const elem = ev.currentTarget;
+   // preview url (the mp3) has ben stashed in the "data-preview-track" attribute.
+   // we need to get that attribute out 
+    const previewURL = elem.dataset.previewTrack;
+    console.log(previewURL);
+    if (previewURL) {
+        audioPlayer.setAudioFile(previewURL);
+        audioPlayer.play();
+    } else {
+        console.log('There is no preview available for this track.')
+    }
+    document.querySelector('footer .track-item').innerHTML = elem.innerHTML;
+};
+
 const getTracks = (term) => {
-    console.log(`
-        get tracks from spotify based on the search term
-        "${term}" and load them into the #tracks section 
-        of the DOM...`);
+    let url = `https://www.apitutor.org/spotify/simple/v1/search?type=track&q=${term}&limit=5`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            document.querySelector('#tracks').innerHTML = '';
+            for (const track of data) {
+                let template = '';
+                if (!track.preview_url) {
+                     template = `
+                     <section class="track-item preview" data-preview-track="${track.preview_url}">
+                        <img src="${track.album.image_url}">
+                        <div class="label">
+                             <h3>${track.name}</h3>
+                            <p>
+                                ${track.artist.name} (No preview available)
+                            </p>
+                        </div>
+                     </section> `
+                } else {
+                    template = `
+                     <section class="track-item preview" data-preview-track="${track.preview_url}">
+                        <img src="${track.album.image_url}">
+                        <i class="fas play-track fa-play" aria-hidden="true"></i>
+                        <div class="label">
+                             <h3>${track.name}</h3>
+                            <p>
+                                ${track.artist.name}
+                            </p>
+                        </div>
+                     </section> `;
+                }
+                document.querySelector('#tracks').innerHTML += template;
+                console.log(track);
+            }
+
+            for (const elem of document.querySelectorAll('.track-item.preview')) {
+                elem.onclick = playTrack;
+            }
+        })
 };
 
 const getAlbums = (term) => {
-    console.log(`
-        get albums from spotify based on the search term
-        "${term}" and load them into the #albums section 
-        of the DOM...`);
+    let url = `https://www.apitutor.org/spotify/simple/v1/search?type=album&q=${term}`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            document.querySelector('#albums').innerHTML = '';
+            for (const album of data) {
+                let template = '';
+                if (!album.image_url) {
+                     template = `
+                <section class="album-card" id="${album.id}">
+                    <div>
+                        <img src="${album.image_url}"> 
+                        <h3>${album.name} No albums were returned </h3>
+                        <div class="footer">
+                            <a href="${album.spotify_url}" target="_blank">
+                                view on spotify
+                            </a>
+                        </div>
+                    </div>
+                </section>`
+            } else {
+                 template = `
+                <section class="album-card" id="${album.id}">
+                    <div>
+                        <img src="${album.image_url}"> 
+                        <h3>${album.name}</h3>
+                        <div class="footer">
+                            <a href="${album.spotify_url}" target="_blank">
+                                view on spotify
+                            </a>
+                        </div>
+                    </div>
+                </section>`;
+            }
+                document.querySelector('#albums').innerHTML += template;
+                console.log(album);
+            }
+        })
 };
 
 
@@ -37,7 +122,6 @@ const getArtist = (term) => {
         "${term}" and load the first artist into the #artist section 
         of the DOM...`);
 
- 
     let url = baseURL + `?type=artist&q=${term}&limit=1`;
     console.log(url);
     fetch(url)
@@ -70,3 +154,5 @@ document.querySelector('#search').onkeyup = (ev) => {
         search();
     }
 };
+
+// how do I add an error message when there are no artists returned or when no tracks or albums are returned 
